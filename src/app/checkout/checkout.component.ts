1f1bced3970a3bdbbb0e1db600c88009041cb479
KishorAO <kishor.phawade@anchanto.com>
2021-07-11 20:40:06 +0530

    setValues, patchValues, resetform

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -22,9 +22,40 @@ export class CheckoutComponent implements OnInit {
       quantity: ['',Validators.required],
       terms: ['',Validators.required]
     })
+    // this.checkoutForm = this.fb.group({
+    //   // emailAddress: new FormControl(''),
+    //   // quantity: new FormControl(''),
+    //   // terms: new FormControl('')
+    //   emailAddress: ['kishor@gmail.com',Validators.required],
+    //   quantity: ['2',Validators.required],
+    //   terms: ['true',Validators.required]
+    // })
+
+ 
   }
   checkout() {
     console.log(this.checkoutForm.value);
     
+    
   }
+  edit(){
+    const ObjValue = {
+      emailAddress : 'kishor.phawade@gmail.com',
+      quantity : 4,
+      terms : true
+    }
+    this.checkoutForm.setValue(ObjValue)
+  }
+  editPatch() {
+    const ObjValue = {
+      emailAddress : 'kishor.phawade@gmail.com',
+      quantity : 4,
+      // terms : true
+    }
+    this.checkoutForm.patchValue(ObjValue)
+  }
+  clear() {
+    this.checkoutForm.reset()
+  }
+
 }
